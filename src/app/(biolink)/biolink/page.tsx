d8adb3befdd92b1eef90d4d959a0329013b18089
Thiago Lins <falecom@thiagolins.dev.br>
2024-04-10 02:27:03 -0300

    remove storybook

--- src/app/(biolink)/biolink/page.tsx
+++ src/app/(biolink)/biolink/page.tsx
@@ -1,10 +1,6 @@
-import Main from '@views/Home/Main'
 interface IProps {
   title?: string
 }
-import BioLinkThemeWrapper from '@layout/BioLinkLayout'
-import { Page } from '@stories/Page'
-import { ReactElement } from 'react'
 export default function Biolink() {
   return <div>LAYOUT DE BIOLINK</div>
 }
